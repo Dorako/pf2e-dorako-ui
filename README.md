0e37a656ddabb6cc7728a7a1b0904c12b56ecc47
Dorako <dirako@gmail.com>
2022-02-02 19:10:02 +0100

    No settings, but otherwise OK

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # pf2e Dorako UI
 This is a UI overhaul for the Pathfinder 2nd Edition System for Foundry VTT.
 
-The theme makes extensive use of `backdrop-filter` which is not supported in current versions of Firefox.
+An opinionated theme inspired by Paizo colors, compatible with several modules for increased consistensy.
+
+Options to coming later.
--- scripts/dorako-ui.js
+++ scripts/dorako-ui.js
@@ -1,41 +1,131 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
 
-Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+Hooks.on("ready", async function () {
+    jQuery.fx.off = true;
 });
 
-Hooks.on("ready", function() {
-  console.log("This code runs once core initialization is ready and game data is available.");
-});
+
+// Hooks.on('getSceneControlButtons', (controls) => {
+// 	const isGM = game.user.isGM;
+// 	controls.push({
+// 		name: 'myName',
+// 		title: "Dorako Button",
+// 		icon: 'fas fa-adjust',
+// 		visible: isGM,
+// 		layer: 'myLayer',
+// 		activeTool: 'select',
+// 		flags: {
+// 		},
+// 		tools: [
+// 			{
+// 				name: 'select',
+// 				title: 'mySelect',
+// 				icon: 'fas fa-eye'
+// 			}
+// 		]
+// 	});
+// });
+
+// Hooks.on('renderSceneControls', (controls) => {
+// 	if (canvas != null && canvas.terrain) {
+// 		canvas.terrain.visible = (canvas.terrain.showterrain || controls.activeControl == 'terrain');
+
+// 		if (controls.activeControl == 'terrain') {
+// 			if (canvas.terrain.toolbar == undefined)
+// 				canvas.terrain.toolbar = new TerrainLayerToolBar();
+// 			canvas.terrain.toolbar.render(true);
+// 			//$('#terrainlayer-tools').toggle(controls.activeTool == 'addterrain');
+// 		} else {
+// 			if (!canvas.terrain.toolbar)
+// 				return;
+// 			canvas.terrain.toolbar.close();
+// 		}
+// 	}
+// });
+// Hooks.on('renderTerrainLayerToolBar', () => {
+// 	const tools = $(canvas.terrain.toolbar.form).parent();
+// 	if (!tools)
+// 		return;
+// 	if (isNewerVersion(game.version, "9")) {
+// 		const controltools = $('li[data-tool="addterrain"]').closest('.sub-controls');
+// 		const offset = controltools.offset();
+// 		tools.css({ top: `${offset.top}px`, left: `${offset.left + controltools.width()}px` });
+// 	} else {
+// 		const controltools = $('li[data-control="terrain"] ol.control-tools');
+// 		const offset = controltools.offset();
+// 		tools.css({ top: `${offset.top}px`, left: `${offset.left + controltools.width() + 6}px` });
+//     }
+// });
+
 
 Hooks.on('init', () => {
 	// Register module settings.
 
-	game.settings.register('dorako-ui', 'myToggle', {
-		name: "My Toggle Name",
-		hint: "My Toggle Hint",
+	// game.settings.register("pf2e-dorako-ui", "stockColor", {
+    //     name: "Stock Color",
+    //     hint: "Stock Color Picker Hunt",
+	// 	scope: "world",
+    //     config: true,
+    //     type: String,
+    //     default: "#ffffff",
+    //     onChange: () => refresh()
+    // });
+
+	// new window.Ardittristan.ColorSetting("pf2e-dorako-ui", "primaryColor", {
+	// 	name: "Primary Color",      // The name of the setting in the settings menu
+	// 	hint: "Used for active controls and player-accesible scenes",   // A description of the registered setting and its behavior
+	// 	label: "Click",         // The text label used in the button
+	// 	restricted: false,             // Restrict this setting to gamemaster only?
+	// 	defaultColor: "rgb(94, 0, 0)",     // The default color of the setting
+	// 	scope: "client",               // The scope of the setting
+	// 	onChange: (value) => {
+	// 		let newColor = game.settings.get("pf2e-dorako-ui", "primaryColor")
+	// 		console.log(newColor);
+	// 		const root = document.querySelector(':root').style;
+	// 		root.setProperty("--paizo-red", newColor);
+	// 	}
+	// })
+
+	// new window.Ardittristan.ColorSetting("pf2e-dorako-ui", "secondaryColor", {
+	// 		name: "Secondary Color",      // The name of the setting in the settings menu
+	// 		hint: "Used for toggles and hidden scenes",   // A description of the registered setting and its behavior
+	// 		label: "Click",         // The text label used in the button
+	// 		restricted: false,             // Restrict this setting to gamemaster only?
+	// 		defaultColor: "rgb(23, 31, 105)",     // The default color of the setting
+	// 		scope: "client",               // The scope of the setting
+	// 		onChange: (value) => {
+	// 			let newColor = game.settings.get("pf2e-dorako-ui", "secondaryColor")
+	// 			console.log(newColor);
+	// 			const root = document.querySelector(':root').style;
+	// 			root.setProperty("--paizo-blue", newColor);
+	// 		}        // A callback function which triggers when the setting is changed
+	// 	})
+
+	// game.settings.register('pf2e-dorako-ui', 'myToggle', {
+	// 	name: "Dark Theme",
+	// 	hint: "My Toggle Hint",
+	// 	scope: "world",
+	// 	type: Boolean,
+	// 	default: false,
+	// 	config: true,
+	// 	onChange: () => {
+	// 		location.reload();
+	// 	}
+	// });
+
+	game.settings.register('pf2e-dorako-ui', 'skin-modules', {
+		name: "Skin popular modules?",
+		hint: "Applies consistent theming to various modules (Disabling can have unintended consequenses)",
 		scope: "world",
 		type: Boolean,
-		default: false,
+		default: true,
 		config: true,
 		onChange: () => {
 			location.reload();
 		}
 	});
 
-	game.settings.register('dorako-ui', 'skin-custom-hotbar', {
-		name: "Skin Custom Hotbar module?",
-		hint: "Applies a similar styling to the hotbar provided by the Custom Hotbar module.",
-		scope: "world",
-		type: Boolean,
-		default: false,
-		config: true,
-		onChange: () => {
-			location.reload();
-		}
-	});
 
-	game.settings.register('dorako-ui', 'disable-all-styles', {
+	game.settings.register('pf2e-dorako-ui', 'disable-all-styles', {
 		name: "Disable all styles?",
 		hint: "Ignore all the toggles and removes any effect of the module, without having to disable it.",
 		scope: "client",
@@ -71,48 +161,49 @@ Hooks.on('init', () => {
 	// 	}
 	// }
 
-	if (!game.settings.get('dorako-ui', 'disable-all-styles')) {
+	if (!game.settings.get('pf2e-dorako-ui', 'disable-all-styles')) {
 		injectBaseCss()
+		skinChat()
+		skinNavigation()
+		skinControls()
+		skinTokenHud()
 
-		if (game.settings.get('dorako-ui', 'highlightGmOwnerText')) { injectOptionalCss() }
-		if (game.settings.get('dorako-ui', 'skin-custom-hotbar')) { injectCustomHotbarCss() }
-
-		if (game.settings.get('dorako-ui', 'myToggle')) {
-			console.log("myToggle was turned ON when script loaded")
-		} else {
-				console.log("myToggle was turned OFF when script loaded")
+		if (game.settings.get('pf2e-dorako-ui', 'skin-modules')) {
+			skinCustomHotbar()
+			skinTokenActionHud()
+			skinWindowControls()
 		}
 	}
 
 });
 
-Hooks.on('getSceneNavigationContext', () => {
-	if (!game.settings.get('dorako-ui', 'navigationVerticalToggle')) {
-		navigation = document.querySelector("nav.app > ol#scene-list");
-		if (navigation) {
-			navigation.classList.add("vertical")
-		}
-	}
-	if (game.settings.get('dorako-ui', 'myToggle')) {
-		addClassByQuerySelector("compact-mode", "body")
-	}
-});
+// Hooks.on('getSceneNavigationContext', () => {
+// 	if (!game.settings.get('dorako-ui', 'navigationVerticalToggle')) {
+// 		navigation = document.querySelector("nav.app > ol#scene-list");
+// 		if (navigation) {
+// 			navigation.classList.add("vertical")
+// 		}
+// 	}
+// 	if (game.settings.get('dorako-ui', 'myToggle')) {
+// 		addClassByQuerySelector("compact-mode", "body")
+// 	}
+// });
 
-Hooks.on('renderCombatCarousel', () => {
-	let carouselSize = game.settings.get('combat-carousel', 'carouselSize')
-	if (carouselSize !== "") {
-		addClassByQuerySelector(carouselSize, "#combat-carousel")
-	}
-});
+// Hooks.on('renderCombatCarousel', () => {
+// 	let carouselSize = game.settings.get('combat-carousel', 'carouselSize')
+// 	if (carouselSize !== "") {
+// 		addClassByQuerySelector(carouselSize, "#combat-carousel")
+// 	}
+// });
 
-Hooks.on('renderSidebarTab', async (object, html) => {
-	if (object instanceof Settings) {
-	  const details = html.find('#game-details')
-	  const list = document.createElement('ul')
-	  list.innerHTML = await renderTemplate('modules/dorako-ui/templates/settings-info.hbs')
-	  details.append(list.firstChild)
-	}
-  })
+// Hooks.on('renderSidebarTab', async (object, html) => {
+// 	if (object instanceof Settings) {
+// 	  const details = html.find('#game-details')
+// 	  const list = document.createElement('ul')
+// 	  list.innerHTML = await renderTemplate('modules/pd2e-dorako-ui/templates/settings-info.hbs')
+// 	  details.append(list.firstChild)
+// 	}
+//   })
 
 
 function addClassByQuerySelector(className, selector) {
@@ -127,64 +218,82 @@ function injectBaseCss() {
 	const mainCss = document.createElement("link");
 	mainCss.setAttribute("rel", "stylesheet")
 	mainCss.setAttribute("type", "text/css")
-	mainCss.setAttribute("href", "modules/dorako-ui/css/dorako-ui.css")
+	mainCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/dorako-ui.css")
 	mainCss.setAttribute("media", "all")
 	head.insertBefore(mainCss, head.lastChild);
 }
 
 // Core UI
 
-function skinControls() {
-	const head = document.getElementsByTagName("head")[0];
-	const newCss = document.createElement("link");
-	newCss.setAttribute("rel", "stylesheet")
-	newCss.setAttribute("type", "text/css")
-	newCss.setAttribute("href", "modules/dorako-ui/css/controls.css")
-	newCss.setAttribute("media", "all")
-	head.insertBefore(newCss, head.lastChild);
-}
-
 function skinChat() {
 	const head = document.getElementsByTagName("head")[0];
 	const newCss = document.createElement("link");
 	newCss.setAttribute("rel", "stylesheet")
 	newCss.setAttribute("type", "text/css")
-	newCss.setAttribute("href", "modules/dorako-ui/css/chat.css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/chat.css")
 	newCss.setAttribute("media", "all")
 	head.insertBefore(newCss, head.lastChild);
 }
+
+function skinControls() {
+	const head = document.getElementsByTagName("head")[0];
+	const newCss = document.createElement("link");
+	newCss.setAttribute("rel", "stylesheet")
+	newCss.setAttribute("type", "text/css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/controls.css")
+	newCss.setAttribute("media", "all")
+	head.insertBefore(newCss, head.lastChild);
+}
+
+function skinNavigation() {
+	const head = document.getElementsByTagName("head")[0];
+	const newCss = document.createElement("link");
+	newCss.setAttribute("rel", "stylesheet")
+	newCss.setAttribute("type", "text/css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/navigation.css")
+	newCss.setAttribute("media", "all")
+	head.insertBefore(newCss, head.lastChild);
+}
+
+function skinTokenHud() {
+	const head = document.getElementsByTagName("head")[0];
+	const newCss = document.createElement("link");
+	newCss.setAttribute("rel", "stylesheet")
+	newCss.setAttribute("type", "text/css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/token-hud.css")
+	newCss.setAttribute("media", "all")
+	head.insertBefore(newCss, head.lastChild);
+}
+
 
 // Modules
 
-function skinCustomHotbarCss() {
+function skinCustomHotbar() {
 	const head = document.getElementsByTagName("head")[0];
 	const newCss = document.createElement("link");
 	newCss.setAttribute("rel", "stylesheet")
 	newCss.setAttribute("type", "text/css")
-	newCss.setAttribute("href", "modules/dorako-ui/css/custom-hotbar.css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/custom-hotbar.css")
 	newCss.setAttribute("media", "all")
 	head.insertBefore(newCss, head.lastChild);
 }
 
-function skinTokenActionHudCss() {
+function skinTokenActionHud() {
 	const head = document.getElementsByTagName("head")[0];
 	const newCss = document.createElement("link");
 	newCss.setAttribute("rel", "stylesheet")
 	newCss.setAttribute("type", "text/css")
-	newCss.setAttribute("href", "modules/dorako-ui/css/token-action-hud.css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/token-action-hud.css")
 	newCss.setAttribute("media", "all")
 	head.insertBefore(newCss, head.lastChild);
 }
 
-function skinWindowControl() {
+function skinWindowControls() {
 	const head = document.getElementsByTagName("head")[0];
 	const newCss = document.createElement("link");
 	newCss.setAttribute("rel", "stylesheet")
 	newCss.setAttribute("type", "text/css")
-	newCss.setAttribute("href", "modules/dorako-ui/css/window-control.css")
+	newCss.setAttribute("href", "modules/pf2e-dorako-ui/styles/window-control.css")
 	newCss.setAttribute("media", "all")
 	head.insertBefore(newCss, head.lastChild);
 }
-
-
-
